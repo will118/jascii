309f275cfa0f9f1d141ef3bef8dc61c301ab4369
Will <will118@gmail.com>
2016-02-10 20:45:44 +0000

    Initial commit

---
+++ process_png_frames.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const ImageToAscii = require('image-to-ascii');
+
+const inputPath = 'input_frames';
+const outputPath = 'output_frames';
+
+(function asciiLoop(index) {
+  const filename = `${index}.png`;
+  const imagePath = `${inputPath}/${filename}`;
+  const options = {
+    path: imagePath,
+    pxWidth: 1,
+    size: {
+      width: 70,
+      height: 40
+    }
+  };
+
+  fs.stat(imagePath, (err, stat) => {
+    if (stat) {
+      ImageToAscii(options, (err, data) => {
+        if (err) throw err;
+        const asciiFilename = filename.replace('png', 'txt');
+        fs.writeFile(`${outputPath}/${asciiFilename}`, data, (err) => {
+          if (err) throw err;
+          asciiLoop(index + 1);
+        });
+      });
+    }
+  });
+})(1);
