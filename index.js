395546de8535eb6a1486b45ae4b7b26d6699df42
Will <will118@gmail.com>
2016-02-10 20:52:19 +0000

    Makes it a function which writes to a stream instead of a server.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const http = require('http');
 const fs = require('fs');
 
 function getFrame(index, cb) {
@@ -11,24 +10,20 @@ function getFrame(index, cb) {
   });
 }
 
-const server = http.createServer(function(request, response) {
-  response.setHeader('Transfer-Encoding', 'chunked');
-  response.write('\033[2J');
-  response.write('\033[200B');
-  response.write('\033[2H');
+function jascii(stream) {
+  stream.write('\033[2J');
+  stream.write('\033[200B');
+  stream.write('\033[2H');
 
   (function loop(index) {
     getFrame(index, (err, frame) => {
       if (frame) {
-        response.write('\033[2H');
-        response.write(frame);
+        stream.write('\033[2H');
+        stream.write(frame);
         setTimeout(loop, 66, index + 1);
       } else {
         loop(1);
       }
     });
   })(1);
-
-}).listen(1337);
-
-console.log('server running');
+}
