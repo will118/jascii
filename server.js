2870b259414828f0d7af6bcd4327a07b02e9cabd
Will <will118@gmail.com>
2016-02-10 20:47:27 +0000

    Removes toilet stuff

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const http = require('http');
 const fs = require('fs');
-const exec = require('child_process').exec;
 
 function getFrame(index, cb) {
   fs.readFile(`output_frames/${index}.txt`, 'utf8', (err, data) => {
@@ -12,31 +11,23 @@ function getFrame(index, cb) {
   });
 }
 
-function toiletInvoke(msg, cb) {
-  var cmd = 'toilet -S -w 70 -f mono12 -F metal ' + msg;
-  exec(cmd, function(error, stdout, stderr) { cb(stdout); });
-}
-
 const server = http.createServer(function(request, response) {
   response.setHeader('Transfer-Encoding', 'chunked');
   response.write('\033[2J');
   response.write('\033[200B');
   response.write('\033[2H');
 
-  toiletInvoke('CRAPTECH', crapTech => {
-    (function loop(index) {
-      getFrame(index, (err, frame) => {
-        if (frame) {
-          response.write('\033[2H');
-          response.write(crapTech);
-          response.write(frame);
-          setTimeout(loop, 66, index + 1);
-        } else {
-          loop(1);
-        }
-      });
-    })(1);
-  });
+  (function loop(index) {
+    getFrame(index, (err, frame) => {
+      if (frame) {
+        response.write('\033[2H');
+        response.write(frame);
+        setTimeout(loop, 66, index + 1);
+      } else {
+        loop(1);
+      }
+    });
+  })(1);
 
 }).listen(1337);
 
