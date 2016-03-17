5a545e470cce7ffbc075242f5406e0be14935e72
Will <will118@gmail.com>
2016-03-17 15:44:06 +0100

    Only listen on localhost

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const jascii = require('./ascii');
 http.createServer((request, response) => {
   response.setHeader('Transfer-Encoding', 'chunked');
   jascii(response);
-}).listen(3000);
+}).listen(3000, 'localhost');
